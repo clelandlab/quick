5914bf36d5f97a069f564c475740e3ce471f9084
Lab Web <30175757+phantomlsh@users.noreply.github.com>
2026-04-10 15:57:58 -0500

    Update qick to 0.2.401

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(where="."),
     python_requires=">=3.8, <4",
     install_requires=[
-        "qick==0.2.366",
+        "qick==0.2.401",
         "numpy",
         "scipy",
         "pyyaml",
